6bc5dfe8d9a097cabf32a8cf3b4ed9afa81d643e
Darren <drnare>
2025-07-16 17:42:06 +0100

    update colors to speccy palette

--- src/colors.ts
+++ src/colors.ts
@@ -6,48 +6,83 @@ export type Color = {
 
 export const colors = [
   {
-    name: 'Blue',
+    name: 'BlueDark',
     value: 'B',
-    color: '#00F',
+    color: '#0100ce',
+  },
+  {
+    name: 'Blue',
+    value: 'b',
+    color: '#0200fd',
+  },
+  {
+    name: 'RedDark',
+    value: 'R',
+    color: '#cf0100',
   },
   {
     name: 'Red',
-    value: 'R',
-    color: '#F00',
+    value: 'r',
+    color: '#ff0201',
   },
   {
-    name: 'Yellow',
-    value: 'Y',
-    color: '#FF0',
+    name: 'MagentaDark',
+    value: 'M',
+    color: '#cf01ce',
+  },
+  {
+    name: 'Magenta',
+    value: 'm',
+    color: '#ff02fd',
+  },
+  {
+    name: 'GreenDark',
+    value: 'G',
+    color: '#00cf15',
   },
   {
     name: 'Green',
-    value: 'G',
-    color: '#0F0',
+    value: 'g',
+    color: '#00ff1c',
   },
   {
-    name: 'Orange',
-    value: 'O',
-    color: '#F70',
+    name: 'CyanDark',
+    value: 'C',
+    color: '#01cfcf',
+  },
+  {
+    name: 'Cyan',
+    value: 'c',
+    color: '#02ffff',
+  },
+  {
+    name: 'YellowDark',
+    value: 'Y',
+    color: '#cfcf15',
+  },
+  {
+    name: 'Yellow',
+    value: 'y',
+    color: '#ffff1d',
+  },
+  {
+    name: 'WhiteDark',
+    value: 'W',
+    color: '#cfcfcf',
   },
   {
     name: 'White',
-    value: 'W',
-    color: '#FFF',
+    value: 'w',
+    color: '#fff',
   },
   {
-    name: 'Purple',
-    value: 'P',
-    color: '#F0F',
+    name: 'OrangeDark',
+    value: 'O',
+    color: '#ff7f00',
   },
   {
-    name: 'Turquoise',
-    value: 'T',
-    color: '#0FF',
-  },
-  {
-    name: 'Silver',
-    value: 'S',
-    color: '#777',
+    name: 'Orange',
+    value: 'o',
+    color: '#fa9c1b',
   },
 ]
